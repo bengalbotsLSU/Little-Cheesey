30cecf3a380c8d63a158383838213172d2c1cf31
bengalbotsLSU <91845028+bengalbotsLSU@users.noreply.github.com>
2022-10-12 10:56:27 -0500

    Delete led_test.py

--- led_test.py
+++
@@ -1,9 +0,0 @@
-import Setup
-import MotorCallableFunctions
-from time import sleep
-
-MotorCallableFunctions.killMotors()
-sleep(5)
-MotorCallableFunctions.start()
-sleep(5)
-print(Setup._pi.read(22))
